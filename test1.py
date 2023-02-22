295ffec2f3c6027916779133a94a41f33c1e95fc
Swarnim Burnwal <swarnimburnwal3@gmail.com>
2023-02-22 22:21:08 +0530

    .

--- test1.py
+++ test1.py
@@ -1,54 +1,13 @@
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-
-# Load the data
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
-num_classes = 10
-
-# Preprocess the data
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-
-# decrease the size of the images
-
-train_images = train_images[:1000]
-train_labels = train_labels[:1000]
-
-
-import cv2
+from keras import datasets, layers, models
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
 import numpy as np
-
-train_images = np.array([cv2.resize(img, (128, 128)) for img in train_images])
-test_images = np.array([cv2.resize(img, (128, 128)) for img in test_images])
-print(train_labels.shape)
-# Define the CNN model
-model = Sequential([
-  Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
-  MaxPooling2D((2, 2)),
-  Conv2D(64, (3, 3), activation='relu'),
-  MaxPooling2D((2, 2)),
-  Conv2D(128, (3, 3), activation='relu'),
-  MaxPooling2D((2, 2)),
-  Conv2D(128, (3, 3), activation='relu'),
-  MaxPooling2D((2, 2)),
-  Flatten(),
-  Dense(512, activation='relu'),
-  Dense(1, activation='softmax')
-])
-
-# Compile the model
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-# Train the model
-model.fit(train_images, train_labels, epochs=10, batch_size=32, validation_data=(test_images, test_labels))
-
-# Save the trained model
-model.save('face_recognition_model.h5')
-
-# Load the trained model
-model = tf.keras.models.load_model('face_recognition_model.h5')
-
-# Use the model to predict the identity of a new face image
+import matplotlib.pyplot as plt
+import os
+from time import sleep
+import cv2
+import pandas as pd
+from keras.optimizers import SGD
